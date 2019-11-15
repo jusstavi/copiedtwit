2a713f0e846440665f62bfe3113632caaf12f176
jusstavi <56784418+jusstavi@users.noreply.github.com>
2019-11-15 23:14:49 +0530

    Update my_tweepy.py

--- my_tweepy.py
+++ my_tweepy.py
@@ -5,4 +5,4 @@ def auth():
 	auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
 	auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
 	return tweepy.API(auth, retry_count=10, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-	return
+	
